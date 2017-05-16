67750c86c88cea34394a1747c85f196805ced0a1
Alda Luong <alda@optimizely.com>
2017-05-16 14:35:06 -0700

    Manual testing with universal framework.

--- Mobile Buy SDK Sample Apps/Advanced App - ObjC/Mobile Buy SDK Advanced Sample/AppDelegate.m
+++ Mobile Buy SDK Sample Apps/Advanced App - ObjC/Mobile Buy SDK Advanced Sample/AppDelegate.m
@@ -36,16 +36,147 @@
     // ---- Initialize Optimizely ----
     self.optlyManager = [OPTLYManager init:^(OPTLYManagerBuilder * _Nullable builder) {
         
-        builder.projectId = @"8174444717";
+        builder.projectId = @"8356170303"; //ShopifyManualTestingObjCSDK
+        
+        // ---- Object Manager Initialization ----
+        // check that the modules are set properly
+        // Custom Logger
+        builder.logger = [[OPTLYLoggerDefault alloc] initWithLogLevel:OptimizelyLogLevelDebug];
+        // Custom Error Handler
+        //builder.errorHandler = [OPTLYErrorHandlerDefault new];
+        // Custom Event Dispatcher
+        //builder.eventDispatcher = [OPTLYEventDispatcherNoOp new];
+        // Custom User Profile
+        //builder.userProfile = [OPTLYUserProfileNoOp new];
+        // Custom Datafile Manager
+        //builder.datafileManager = [OPTLYDatafileManagerNoOp new];
         
     }];
     
+    
     [self.optlyManager initializeWithCallback:^(NSError * _Nullable error, OPTLYClient * _Nullable client) {
         
-        OPTLYVariation *variation = [client activate:@"ColorThemeExperiment" userId:@"alda" attributes:@{@"buyerType":@"frequent"}];
+//        OPTLYVariation *variation = [client activate:@"ColorThemeExperiment" userId:@"alda" attributes:@{@"buyerType":@"frequent"}];
+//        
+//        NSLog(@"*********** User was bucketed into %@ variation. ***********", variation.variationKey);
         
-        NSLog(@"*********** User was bucketed into %@ variation. ***********", variation.variationKey);
+        // Activate invalid experiment
+        //OPTLYVariation *variation = [client activate:@"invalidExperiment" userId:@"alda"];
+        
+        // Activate no audience, no user attributes
+        //OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda1"];
+        
+        // Activate with audience off, user attributes
+        //OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda2" attributes:@{@"abc":@"123"}];
+        
+        // Activate with audience on, user attributes match (variations: ewa_var1, ewa_var2)
+        //OPTLYVariation *variation = [client activate:@"ExperimentWithAudience" userId:@"alda3" attributes:@{@"testAttribute1":@"testAttributeValue1"}];
+        
+        // Activate with audience on, user attributes no match
+        //OPTLYVariation *variation = [client activate:@"ExperimentWithAudience" userId:@"alda4" attributes:@{@"testAttribute1":@"testAttributeValueBad"}];
+        
+        // Activate with invalid attribute (not in datafile)
+        //OPTLYVariation *variation = [client activate:@"ExperimentWithAudience" userId:@"alda5" attributes:@{@"abc":@"123"}];
+        //OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda6" attributes:@{@"abc":@"123"}];
+        
+        // Activate experiment in group (variation: ega_var1, ega_var2)
+        // * Confirm that either ExperimentInGroupA_1 or ExperimentInGroupA_2 is bucketed
+        // * The experiment is bucketed according to the group and user id
+        //OPTLYVariation *variation = [client activate:@"ExperimentInGroupA_1" userId:@"alda7"];
+        // (variation: ega_var3, ega_var4)
+        //OPTLYVariation *variation = [client activate:@"ExperimentInGroupA_2" userId:@"alda7"];
+    
+        // Whitelisted, user in group (ena_var1)
+        //OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda8"];
+        
+        //Track invalid event
+        //[client track:@"blah" userId:@"alda9"];
+        
+        //Track valid event, whitelisted user (in "ExperimentNoAudience")
+        //OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda8"];
+        //[client track:@"testEvent1" userId:@"alda8"];
+        
+        //Track valid event, whitelisted user (in "ExperimentNoAudience") with revenue
+//        OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda8"];
+//        [client track:@"testEvent1" userId:@"alda8" eventValue:@500]; // --> this API will be deprecated
+//        [client track:@"testEvent1" userId:@"alda8" eventTags:@{@"revenue":@123, @"testEvent1":@"aldaEventTagValue1"}];
+        
+        //Track valid event with revenue
+//        OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda12"];
+//        NSLog(@"bucketed variation: %@", variation.variationKey);
+//        [client track:@"testEvent1" userId:@"alda12" eventValue:@500]; // --> this API will be deprecated
+//        [client track:@"testEvent1" userId:@"alda12" eventTags:@{@"revenue":@123, @"testEvent1":@"aldaEventTagValue1"}];
+        
+        //Track experiment in group
+//        OPTLYVariation *variation = [client activate:@"ExperimentInGroupA_1" userId:@"alda13"];
+//        NSLog(@"bucketed variation 1: %@", variation.variationKey);
+//        variation = [client activate:@"ExperimentInGroupA_2" userId:@"alda13"];
+//        NSLog(@"bucketed variation 2: %@", variation.variationKey);
+//        [client track:@"testEventExperimentInGroup" userId:@"alda13"];
+        
+        //Track with audience off, user attributes (variation1)
+//        OPTLYVariation *variation = [client activate:@"ExperimentNoAudience" userId:@"alda14" attributes:@{@"device_type":@"blah"}];
+//        [client track:@"testEvent1" userId:@"alda14" attributes:@{@"device_type":@"blah"}];
+        
+        //Track with audience on, user attributes match
+//        OPTLYVariation *variation = [client activate:@"ExperimentWithAudience" userId:@"alda15" attributes:@{@"testAttribute1":@"testAttributeValue1"}];
+//        NSLog(@"bucketed variation: %@", variation.variationKey);
+//        [client track:@"testEvent1" userId:@"alda15" attributes:@{@"testAttribute1":@"testAttributeValue1"}];
+        
+         // Track with invalid attribute (not in datafile)
+        OPTLYVariation *variation = [client activate:@"ExperimentWithAudience" userId:@"alda23" attributes:@{@"testAttribute1":@"testAttributeValue1"}];
+        //NSLog(@"bucketed variation: %@", variation.variationKey);
+        [client track:@"testEvent1" userId:@"alda23" attributes:@{@"blah":@"ios"}];
+        
+        //Track with audience on, user attributes no match
+//        OPTLYVariation *variation = [client activate:@"ExperimentWithAudience" userId:@"alda17" attributes:@{@"testAttribute1":@"testAttributeValue1"}];
+//        NSLog(@"bucketed variation: %@", variation.variationKey);
+        //[client track:@"testEvent1" userId:@"alda17" attributes:@{@"testAttribute1":@"123"}];
+        
+        
+        // ---- Get Variation ----
+        // Get Variation valid experiment (variation1)
+        //OPTLYVariation *variation = [client variation:@"experimentNoAudience" userId:@"alda"];
+        //NSLog(@"get variation 1: %@", variation.variationKey);
+        
+        // Invalid experiment (null)
+        //variation = [client variation:kExperimentInvalid userId:kUserID];
+        //NSLog(@"get variation 2: %@", variation.variationKey);
+        
+        // Activate with audience off, user attributes (default)
+        //variation = [client variation:kExperimentNoAudience userId:@"alda1" attributes:@{@"abc":@"123"}];
+        //NSLog(@"get variation 3: %@", variation.variationKey);
+        
+        // Activate with audience on, user attributes match (aud_exp_2)
+        //variation = [client variation:kExperimentUsingAudience userId:@"alda1" attributes:@{@"device_type":@"android"}];
+        //NSLog(@"get variation 4: %@", variation.variationKey);
+        
+        // Activate with audience on, user attributes no match (null)
+        //variation = [client variation:kExperimentUsingAudience userId:@"alda2" attributes:@{@"invalidKey":@"invalidValue"}];
+        //NSLog(@"get variation 5: %@", variation.variationKey);
+        //variation = [client variation:kExperimentUsingAudience userId:@"alda2" attributes:@{@"device_type":@"invalidValue2"}];
+        //NSLog(@"get variation 6: %@", variation.variationKey);
+        
+        // Activate with invalid attribute (not in datafile) (variation1)
+        //variation = [client variation:kExperimentNoAudience userId:@"alda3" attributes:@{@"abc":@"123"}];
+        //NSLog(@"get variation 7: %@", variation.variationKey);
+        
+        // Activate experiment in group (exp_grp_var_1)
+        //variation = [client variation:kExperimentInGroup userId:@"alda5"];
+        //NSLog(@"get variation 8: %@", variation.variationKey);
+        
+        // Whitelisted, user in group (exp_grp_var_1)
+        //variation = [client variation:kExperimentInGroup userId:@"vigraja"];
+        //NSLog(@"get variation 9: %@", variation.variationKey);
+        
+        // Whitelisted, audience on (aud_exp_1)
+        //variation = [client variation:kExperimentUsingAudience userId:@"aliriz"];
+        //NSLog(@"get variation 10: %@", variation.variationKey);
+        
+        //NSLog(@"bucketed variation: %@", variation);
     }];
+    
+    
     
     return YES;
 }
